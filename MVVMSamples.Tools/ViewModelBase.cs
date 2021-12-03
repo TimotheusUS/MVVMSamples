513c1a23eebf3caa53447c1312a6a813569f7132
tststs789@gmail.com <tststs789@gmail.com>
2021-12-03 20:05:44 +0100

    first commit

--- MVVMSamples.Tools/ViewModelBase.cs
+++ MVVMSamples.Tools/ViewModelBase.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace MVVMSamples.Tools
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : INotifyPropertyChanged 
     {
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));        
